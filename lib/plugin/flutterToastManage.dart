c5f705853edca04785d381f22e8cfaf4fa4b4f5f
liuchuancong <liuchuancong@touscm.com>
2020-08-03 14:26:16 +0800

    更新fluttertoast

--- lib/plugin/flutterToastManage.dart
+++ lib/plugin/flutterToastManage.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
 class FlutterToastManage {
-  showToast(String msg,
-      {int seconds = 1,
-      Color color = Colors.white,
-      ToastGravity gravity = ToastGravity.CENTER}) {
-        Fluttertoast.cancel();
+  showToast(
+    String msg, {
+    int seconds = 1,
+    Color color = Colors.white,
+    ToastGravity gravity = ToastGravity.CENTER,
+  }) {
     Fluttertoast.showToast(
       msg: msg,
       toastLength: Toast.LENGTH_SHORT,
